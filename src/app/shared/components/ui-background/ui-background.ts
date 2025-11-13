6d9e52997f1209167bc66e89409b34b5117561ac
Juan Carlos Hoyos <123840011+JuanCHoyos@users.noreply.github.com>
2025-11-13 16:25:07 -0500

    feat: :sparkles: Add UIBackground component with animated SVG circles

---
+++ src/app/shared/components/ui-background/ui-background.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ui-background',
+  imports: [CommonModule],
+  templateUrl: './ui-background.html',
+  styleUrls: ['./ui-background.scss'],
+})
+export class UIBackground {}
