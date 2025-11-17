ee498a29fc4a7a25d198b465bd80da41be6e65fe
Juan Carlos Hoyos <123840011+JuanCHoyos@users.noreply.github.com>
2025-11-17 14:04:18 -0500

    refactor: :recycle: convert projects section to dynamic data-driven component

--- src/app/features/home/projects/projects.ts
+++ src/app/features/home/projects/projects.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { UIICon, UISeparator, UIHeading } from '../../../shared/components';
 import { HeadingType } from '../../../shared/components/ui-heading/ui-heading';
+
+export type Project = {
+  title: string;
+  description: string;
+  github: string;
+  url: string;
+  image: string;
+  stack: string[];
+};
 
 @Component({
   selector: 'projects',
@@ -13,6 +22,21 @@ import { HeadingType } from '../../../shared/components/ui-heading/ui-heading';
     padding: 0
   }`,
 })
-export class Projects {
+export class Projects implements OnInit {
   HeadingType = HeadingType;
+  projects = signal<Project[]>([]);
+
+  ngOnInit(): void {
+    this.projects.set([
+      {
+        title: 'FormCraftify',
+        description:
+          'Herramienta que permite crear formularios y encuestas mediante un editor visual, con campos configurables y generación dinámica basada en JSON. Desarrollado con Angular, TypeScript y Tailwind.',
+        github: 'https://github.com/JuanCHoyos/FormCraftify',
+        url: 'https://form-craftify-91uz.vercel.app/',
+        image: 'assets/formCraftify.png',
+        stack: ['Angular', 'Typescript', 'Tailwind'],
+      },
+    ]);
+  }
 }
